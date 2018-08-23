f923a7f435598e378f5e4ef0b6c7af5d8a7ea66d
ambsoerensen <ambs91@gmail.com>
2018-08-23 15:36:46 +0200

    add widget class

--- src/ServiceNow/all.ts
+++ src/ServiceNow/all.ts
@@ -3,6 +3,8 @@ export * from './Api';
 export * from './IsysRecord';
 export * from './IsysRelation';
 export * from './IsysScriptInclude';
+export * from './IsysSpWidget';
 export * from './Record';
 export * from './Relation';
 export * from './ScriptInclude';
+export * from './Widget';
