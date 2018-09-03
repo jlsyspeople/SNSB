c23b3ceb46ad9f6decaecc6bef1259de0da1948b
ambsoerensen <ambs91@gmail.com>
2018-09-03 16:15:09 +0200

    cleanup sys_class_name values

--- src/ServiceNow/Record.ts
+++ src/ServiceNow/Record.ts
@@ -18,7 +18,14 @@ export class Record implements IsysRecord
     private _sys_class_name: string;
     public get sys_class_name(): string
     {
-        return this._sys_class_name;
+        let className = this._sys_class_name.toLocaleLowerCase().replace(" ", "_");
+
+        if (className.startsWith("sys_"))
+        {
+            className = className.replace("sys_", "");
+        }
+
+        return className;
     }
 
     private _sys_id: string;
@@ -64,13 +71,13 @@ export class Record implements IsysRecord
     {
         //overwrite to JSON to ensure that json.stringify serializes with public property names and not the private ones. 
         return {
-            sys_class_name: this._sys_class_name,
-            sys_id: this._sys_id,
-            sys_policy: this._sys_policy,
-            sys_updated_on: this._sys_updated_on,
-            sys_created_on: this._sys_created_on,
-            sys_package: this._sys_package,
-            sys_scope: this._sys_scope
+            sys_class_name: this.sys_class_name,
+            sys_id: this.sys_id,
+            sys_policy: this.sys_policy,
+            sys_updated_on: this.sys_updated_on,
+            sys_created_on: this.sys_created_on,
+            sys_package: this.sys_package,
+            sys_scope: this.sys_scope
         };
     }
 }
